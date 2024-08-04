096b9a2c8671eaf9e4dde1f6283401041eb69ad7
BrandonChhin <169076754+BrandonChhin@users.noreply.github.com>
2024-08-04 12:51:14 -0400

    Assign 4 - Part 2 - Declared array of image file names

--- Assignments/Assignment_4/Part_2_Image_gallary/gallery-start/main.js
+++ Assignments/Assignment_4/Part_2_Image_gallary/gallery-start/main.js
@@ -5,14 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
+const imageFiles = ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.jpg"];
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
+for(let i = 0; i < imageFiles.length; i++){
+  const newImage = document.createElement('img');
+  newImage.setAttribute('src', xxx);
+  newImage.setAttribute('alt', xxx);
+  thumbBar.appendChild(newImage);
+}
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
