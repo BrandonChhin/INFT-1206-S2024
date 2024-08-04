b032632a9d4db7c2c51a960dc96c453cb08eafb1
BrandonChhin <169076754+BrandonChhin@users.noreply.github.com>
2024-08-04 12:54:55 -0400

    Assign 4 - Part 1 - Added loop iteration for images

--- Assignments/Assignment_4/Part_2_Image_gallary/gallery-start/main.js
+++ Assignments/Assignment_4/Part_2_Image_gallary/gallery-start/main.js
@@ -5,14 +5,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imageFiles = ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.jpg"];
+const imageFiles = ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.jpg"]
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
-for(let i = 0; i < imageFiles.length; i++){
+for(i = 0; i < imageFiles.length; i++){
   const newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
-  newImage.setAttribute('alt', xxx);
+  newImage.setAttribute('src', `images/pic${i}.jpg`);
+  newImage.setAttribute('alt', `pic${i}`);
   thumbBar.appendChild(newImage);
 }
 
