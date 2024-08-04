6d3978205dfcfdab7d5daf4fb2112ed10f7a861e
BrandonChhin <169076754+BrandonChhin@users.noreply.github.com>
2024-08-04 13:05:21 -0400

    Assign 4 - Part 2 - Fixed imageFiles array and added alt text

--- Assignments/Assignment_4/Part_2_Image_gallary/gallery-start/main.js
+++ Assignments/Assignment_4/Part_2_Image_gallary/gallery-start/main.js
@@ -5,8 +5,16 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const imageFiles = ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.jpg"]
+const imageFiles = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"]
 /* Declaring the alternative text for each image file */
+const altTexts = {
+  'pic1.jpg' : 'Closeup of a human eye',
+  'pic2.jpg' : 'Rock surface',
+  'pic3.jpg' : 'Purple and white flowers',
+  'pic4.jpg' : 'Wall of Pharoah\'s tomb',
+  'pic5.jpg' : 'Moth on a leaf',
+
+}
 
 /* Looping through images */
 for(i = 0; i < imageFiles.length; i++){
